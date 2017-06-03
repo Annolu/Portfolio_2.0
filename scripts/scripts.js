3daba5d842d474f5782fa0aef0384b7cb7c4b873
Noemi <andreoli.noemi@gmail.com>
2017-06-03 16:06:33 +0100

    working on general structure

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,2 +1,42 @@
 $(document).ready(function() {
+
+  setInterval(hideLoader, 200);
+
+  function hideLoader(){
+    var loader= $('.loader-overlayer');
+    loader.addClass('hide-loader');
+  }
+
+  var modal = $('#myModal');
+  var modalButton = $(".modalButton");
+  var span = $(".close");
+
+  modalButton.click(function(e) {
+    modal.addClass('show-modal');
+    fillModal(e);
+  });
+
+  span.click(function() {
+    modal.removeClass('show-modal');
+  });
+
+  $(window).click(function(e) {
+
+    var target= $(e.target);
+
+    if(target.hasClass("menu-item") || target.hasClass("menu-overlayer")){
+      toggleOverlayer(target);
+    }
+
+    if (e.target == modal[0]) {
+      modal.removeClass('show-modal');
+    }
+  })
+
+  function fillModal(e){
+    var originalModalInfo= e.target.parentNode.parentNode.parentNode;
+    var modalInfo= $(originalModalInfo).clone();
+    $(".modal-content").empty();
+    $(".modal-content").append(modalInfo);
+  }
 });
