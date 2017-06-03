5fb405bea0b40f64d4af5909de538d57f30136d7
Noemi <andreoli.noemi@gmail.com>
2017-06-03 18:52:44 +0100

    done general structure

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -39,4 +39,45 @@ $(document).ready(function() {
     $(".modal-content").empty();
     $(".modal-content").append(modalInfo);
   }
+
+  var ctxA = document.getElementById("augusto-chart");
+
+  var data = {
+    labels: ["January", "February", "March", "April", "May", "June", "July"],
+    datasets: [
+      {
+        label: "SKILLZ",
+        backgroundColor: [
+          'rgba(254, 230, 76, 0.8)'
+        ],
+        borderColor: [
+          'rgba(254, 230, 76, 1)'
+        ],
+        borderWidth: 1,
+        data: [6, 9, 7, 8, 7, 6, 7],
+      }
+    ]
+  };
+
+  var chartAugusto = new Chart(ctxA, {
+    type: 'radar',
+    data: data,
+    options: {
+      legend: {
+          position: "bottom"
+      },
+      scale: {
+        ticks: {
+          beginAtZero: true,
+          min: 0,
+          max: 10,
+          stepSize: 2
+        },
+        pointLabels: {
+          fontSize: 14,
+          fontColor: '797878'
+        }
+      }
+    }
+  });
 });
